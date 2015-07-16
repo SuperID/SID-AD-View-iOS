1c7585dc41fb331abfd9c29aafc73905ca751c6e
徐骏杰 <jasee.xu@isncer.com>
2015-07-16 11:49:32 +0800

    update

--- SIDADView/SIDADView.h
+++ SIDADView/SIDADView.h
@@ -12,7 +12,7 @@
 
 /**
  *  带NavigationBar呈现方法：
- *  @param info        使用一登登录后回调的Info，广告视窗的title会根据Info内容使用不同的文案
+ *  @param info        使用一登登录后回调的Info，广告视窗的title会根据Info内容使用不同的文案，传入的内容为用户回调信息的persona部分
  *  @param image       开发者的广告图
  *  @param color       弹出视窗的边框颜色
  *
